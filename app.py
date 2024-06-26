28ccb43a60b2f40e21ec8967ba75296b4cf4ccca
Kim <kimannh@nih.gov>
2024-06-26 16:06:05 -0400

    Assign pcc_code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+import re
 
 app = Flask(__name__)
 
@@ -20,6 +21,19 @@ def home():
     # Call the function and provide the path to your Excel file
     read_excel_and_print_column('ATSB all grants-app FY2021-2024.xlsx')
     return render_template('index.html')
+
+def assign_pcc_code(text):
+    keyword_to_code = {
+        'data science': 'DS',
+        'genomic': 'GP',
+        'imaging technology': 'IT',
+        'nanotechnology': 'NT'
+    }
+    for keyword, code in keyword_to_code.items():
+        if re.search(r'\b{}\b'.format(re.escape(keyword)), text, re.IGNORECASE):
+            return code
+    return None  # return none if no matching keyword found
+
 
 if __name__ == "__main__":
     app.run(debug=True)
